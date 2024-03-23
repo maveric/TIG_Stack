ad309bedd1dc36437413ee2803d36bd3f8d009d9
Neik <25412853+neilgcns@users.noreply.github.com>
2024-03-23 09:26:04 +0000

    Update maid.sh

--- maid.sh
+++ maid.sh
@@ -120,12 +120,11 @@ sleep 2
 ############################## start nodes
 
 sudo env "PATH=$PATH" safenode-manager add --port "$NODE_PORT_FIRST"-$(($NODE_PORT_FIRST+$NUMBER_NODES-1))  --count "$NUMBER_NODES"  --peer "$PEER"  --version "$NODE"
-#sudo env "PATH=$PATH" safenode-manager start  --interval "$DELAY_BETWEEN_NODES"
 
 for i in {1.."$NUMBER_NODES"}
 do
  # your-unix-command-here
-sudo env "PATH=$PATH" safenode-manager start  --service-name safenode$i
+sudo env "PATH=$PATH" safenode-manager start --service-name safenode$i
 sleep "$DELAY_BETWEEN_NODES"
 done
 
