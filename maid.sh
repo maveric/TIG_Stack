9ce291b7439476f9f2d0ea0776c469ad03d029aa
Neik <25412853+neilgcns@users.noreply.github.com>
2024-03-28 23:30:20 +0000

    aded manual delay bettwen nodes

--- maid.sh
+++ maid.sh
@@ -26,7 +26,7 @@ button=black,white
 
 ############################################## select test net action
 
-SELECTION=$(whiptail --title "Safe Network Testnet 1.4" --radiolist \
+SELECTION=$(whiptail --title "Safe Network Testnet 1.5" --radiolist \
 "Testnet Actions                              " 20 70 10 \
 "1" "Install & Start Nodes " OFF \
 "2" "Upgrade Client to Latest" OFF \
@@ -120,7 +120,8 @@ sleep 2
 ############################## start nodes
 
 sudo env "PATH=$PATH" safenode-manager add --node-port "$NODE_PORT_FIRST"-$(($NODE_PORT_FIRST+$NUMBER_NODES-1))  --count "$NUMBER_NODES"  --peer "$PEER"  --version "$NODE"
-sudo env "PATH=$PATH" safenode-manager start --interval "$DELAY_BETWEEN_NODES"
+sudo env "PATH=$PATH" safenode-manager start --interval 11
+#"$DELAY_BETWEEN_NODES"
 
 # FOR USE UNTILL TESTING --INTERVAL IS COMPLETED
 #for ((i=1;i<=$NUMBER_NODES;i++)); do
