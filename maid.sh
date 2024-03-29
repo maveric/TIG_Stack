4a27bab77597c724d89f5224b5a45ab6cd45b47b
Neik <25412853+neilgcns@users.noreply.github.com>
2024-03-29 13:56:44 +0000

    Update maid.sh

--- maid.sh
+++ maid.sh
@@ -26,7 +26,7 @@ button=black,white
 
 ############################################## select test net action
 
-SELECTION=$(whiptail --title "Safe Network Testnet 1.8" --radiolist \
+SELECTION=$(whiptail --title "Safe Network Testnet 1.9" --radiolist \
 "Testnet Actions                              " 20 70 10 \
 "1" "Install & Start Nodes " OFF \
 "2" "Upgrade Client to Latest" OFF \
@@ -120,7 +120,7 @@ sleep 2
 ############################## start nodes
 
 sudo env "PATH=$PATH" safenode-manager add --node-port "$NODE_PORT_FIRST"-$(($NODE_PORT_FIRST+$NUMBER_NODES-1))  --count "$NUMBER_NODES"  --peer "$PEER"  --version "$NODE"
-sudo env "PATH=$PATH" safenode-manager start --interval "$DELAY_BETWEEN_NODES000"
+sudo env "PATH=$PATH" safenode-manager start --interval $DELAY_BETWEEN_NODES000
 
 # FOR USE UNTILL TESTING --INTERVAL IS COMPLETED
 #for ((i=1;i<=$NUMBER_NODES;i++)); do
