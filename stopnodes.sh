c59ae2aa632692a6adcaa96c42dfdb0b1f43080c
Neik <25412853+neilgcns@users.noreply.github.com>
2024-04-07 12:55:21 +0100

    Update stopnodes.sh

--- stopnodes.sh
+++ stopnodes.sh
@@ -1,1 +1,13 @@
+#!/usr/bin/env bash
 
+for i in {56..100}
+do
+ # your-unix-command-here
+ sudo systemctl disable --now safenode$i
+ sudo rm /etc/systemd/system/safenode$i.service
+sudo rm -rf /var/safenode-manager/services/safenode$i
+sudo rm -rf /var/log/safenode/safenode$i
+
+done
+
+sudo systemctl daemon-reload
