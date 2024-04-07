b0856defc0614fefc4261c1b1225de30e0c7c989
Neik <25412853+neilgcns@users.noreply.github.com>
2024-04-07 13:00:47 +0100

    Update stopnodes.sh

--- stopnodes.sh
+++ stopnodes.sh
@@ -11,3 +11,12 @@ sudo rm -rf /var/log/safenode/safenode$i
 done
 
 sudo systemctl daemon-reload
+
+
+# remove NTracking cron jobs temp til NTracking is fixed
+sudo rm /etc/cron.d/ntracking*
+# add cron job for tig stack on 5 min schedule
+echo "*/5 * * * * $USER /usr/bin/mkdir -p /tmp/influx-resources && /bin/bash /usr/bin/influx-resources.sh > /tmp/influx-resources/influx-resources" | sudo tee /etc/cron.d/influx_resources
+
+#install latest infux resources script from github
+sudo rm /usr/bin/influx-resources.sh* && sudo wget -P /usr/bin  https://raw.githubusercontent.com/safenetforum-community/TIG_Stack/main/influx-resources.sh && sudo chmod u+x /usr/bin/influx-resources.sh
