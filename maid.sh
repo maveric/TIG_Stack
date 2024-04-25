0fdfc1099d4be43ac0718b82d4e9e3aa84dc08b2
Neik <25412853+neilgcns@users.noreply.github.com>
2024-04-25 16:47:12 +0100

    Update maid.sh

--- maid.sh
+++ maid.sh
@@ -26,7 +26,7 @@ button=black,white
 
 ############################################## select test net action
 
-SELECTION=$(whiptail --title "Safe Network Testnet 1.0" --radiolist \
+SELECTION=$(whiptail --title "Safe Network Testnet 1.2" --radiolist \
 "Testnet Actions                              " 20 70 10 \
 "1" "Install & Start Nodes " OFF \
 "2" "Upgrade Client to Latest" OFF \
@@ -144,6 +144,7 @@ safe wallet get-faucet "$FAUCET"
 ######################################################################################################################## Stop Nodes
 elif [[ "$SELECTION" == "3" ]]; then
 
+safeup node-manager
 
 sudo env "PATH=$PATH" safenode-manager reset
 rm -rf  ~/.local/share/local_machine/
